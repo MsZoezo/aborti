9ac3beebe01a7a3fd99c068a4cbf8dc83484f660
MsZoezo <zoey@zoezo.dev>
2024-02-26 17:10:02 +0100

    Simple logger added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import { Logger } from "./utils/logger";
 import "dotenv/config"
+
+const logger = new Logger("Index")
 
 const app: Express = express();
 const port = Number(process.env.EXPRESS_PORT) || 8000;
 
 app.get("", (req: Request, res: Response) => res.send("Hello world!"));
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
+app.listen(port, () => logger.info(`Listening on http://localhost:${port}/`));
---
+++ server/src/utils/logger.ts
@@ -0,0 +1,55 @@
+import winston, { addColors } from "winston";
+
+const levels = {
+	error: 1,
+	warn: 2,
+	info: 3,
+	verbose: 4,
+};
+
+addColors({
+	info: "bold cyan",
+	warn: "italic yellow",
+	error: "bold red",
+	verbose: "white",
+});
+
+const logger = winston.createLogger({
+	level: "verbose",
+	levels: levels,
+	format: winston.format.combine(
+		winston.format.metadata(),
+		winston.format.colorize({ message: true, level: false }),
+		winston.format.timestamp({
+			format: "YYYY-MM-DD HH:mm:ss",
+		}),
+        winston.format.printf(({ metadata, timestamp, message }) => `\x1b[90m[${timestamp} ${metadata.context}] ${message}`)
+	),
+	transports: [
+		new winston.transports.Console(),
+	],
+});
+
+export class Logger {
+	private logger: winston.Logger;
+
+	constructor(context: string) {
+		this.logger = logger.child({ context: context });
+	}
+
+	public verbose(message: string): void {
+		this.logger.log("verbose", message);
+	}
+
+	public error(message: string): void {
+		this.logger.log("error", message);
+	}
+
+	public warn(message: string): void {
+		this.logger.log("warn", message);
+	}
+
+	public info(message: string): void {
+		this.logger.log("info", message);
+	}
+}
